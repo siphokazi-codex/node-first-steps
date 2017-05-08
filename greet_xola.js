903a5da507e452964eedc6829bde7b0f65e07aa2
siphokazi-codex <aplenis@webmail.co.za>
2017-05-08 10:45:00 +0200

    Initial Commit

---
+++ greet_xola.js
@@ -0,0 +1,9 @@
+
+const chalk = require('chalk');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+
